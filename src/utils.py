aa1648f665bd1145c0d5a7cff4c6aacfaedc58c8
Aditi Agarwal <aditiagarwal263@gmail.com>
2025-10-01 09:15:00 +0530

    Implement token decoding utilities

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import json
+
+def decode_outputs(predicted_labels, model_type="bert"):
+    entities = []
+    shift_idx = 2 if model_type == "bert" else 0
+
+    for _, elem in enumerate(predicted_labels):
+        attach = False
+        if model_type == "bert" and elem["word"].startswith("##"):
+            attach = True
+        elif model_type == "roberta" and not elem["word"].startswith("Ġ"):
+            attach = True
+
+        if attach and entities:
+            entities[-1]["word"] += elem["word"][shift_idx:]
+            entities[-1]["end"] = elem["end"]
+        else:
+            entities.append({
+                "word": elem["word"],
+                "start": elem["start"],
+                "end": elem["end"],
+                "entity": elem["entity"],
+            })
+            
+    if model_type == "roberta":
+        for elem in entities:
+            if elem["word"].startswith("Ġ"):
+                elem["word"] = elem["word"][1:]
+    return entities
