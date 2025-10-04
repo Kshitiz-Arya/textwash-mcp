2e2b775aeaa8976f72bbd873d03e4c67e4814554
Moumita Dey <moumitade2001@gmail.com>
2025-10-04 11:45:00 +0530

    Create Anonymizer class structure

---
+++ src/anonymizer.py
@@ -0,0 +1,10 @@
+class Anonymizer:
+    def __init__(self, config, classifier):
+        self.config = config
+        self.classifier = classifier
+
+    def get_identifiable_tokens(self, text_input):
+        pass
+
+    def anonymize(self, text_input):
+        return text_input
