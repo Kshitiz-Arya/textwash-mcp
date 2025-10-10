f6099378e3e5c601ed6cd75f13599dbbfdaaf9c1
Aditi Agarwal <aditiagarwal263@gmail.com>
2025-10-10 13:00:00 +0530

    Integrate NER classifier into Anonymizer

--- src/anonymizer.py
+++ src/anonymizer.py
@@ -1,10 +1,17 @@
+from .utils import decode_outputs
+
 class Anonymizer:
     def __init__(self, config, classifier):
         self.config = config
         self.classifier = classifier
 
     def get_identifiable_tokens(self, text_input):
-        pass
+        predictions = decode_outputs(
+            self.classifier(text_input), model_type=self.config.model_type
+        )
+        return predictions
 
     def anonymize(self, text_input):
+        # TODO: Implement replacement
+        tokens = self.get_identifiable_tokens(text_input)
         return text_input
