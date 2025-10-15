5f2ebbd3b766fbb49f2fc80d16abc32b01ae0ce9
Moumita Dey <moumitade2001@gmail.com>
2025-10-15 15:45:00 +0530

    Implement basic string replacement logic

--- src/anonymizer.py
+++ src/anonymizer.py
@@ -1,20 +1,21 @@
+import re
 from .utils import decode_outputs
 
 class Anonymizer:
     def __init__(self, config, classifier):
         self.config = config
         self.classifier = classifier
-        
         with open(self.config.path_to_months_file, "r") as f:
             self.months = [m.strip() for m in f.readlines()]
         with open(self.config.path_to_written_numbers_file, "r") as f:
             self.written_numbers = [w.strip() for w in f.readlines()]
 
-    def get_identifiable_tokens(self, text_input):
-        predictions = decode_outputs(
-            self.classifier(text_input), model_type=self.config.model_type
-        )
-        return predictions
-
     def anonymize(self, text_input):
+        predictions = decode_outputs(self.classifier(text_input), model_type=self.config.model_type)
+        entities = {p["word"]: p["entity"] for p in predictions if p["entity"] != "NONE"}
+        
+        # Simple replacement
+        for word, ent in entities.items():
+            text_input = text_input.replace(word, f"<{ent}>")
+            
         return text_input
